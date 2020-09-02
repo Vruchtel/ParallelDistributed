ed6f9e0f0cd9f493d15179d03532c07722828c6b
Серафима Вручтель <s.vruchtel@cian.ru>
2020-09-02 10:31:41 +0300

    fix tab

--- sem01/1_hello_world/hello_world.c
+++ sem01/1_hello_world/hello_world.c
@@ -2,22 +2,22 @@
 #include <mpi.h>
 
 int main(int argc, char *argv[]) {
-	MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv);
 	
-	int procid, num_procs;
-	MPI_Comm_rank(MPI_COMM_WORLD, &procid);
-	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    int procid, num_procs;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procid);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
     
     char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_length;
-	MPI_Get_processor_name(processor_name, &name_length);
+    int name_length;
+    MPI_Get_processor_name(processor_name, &name_length);
 	
-	printf("Hello, World! My id is %d and my processor name is %s\n", procid, processor_name);
+    printf("Hello, World! My id is %d and my processor name is %s\n", procid, processor_name);
 	
-	if (procid == 0) {
-		printf("All processes count: %d\n", num_procs);
-	}
+    if (procid == 0) {
+        printf("All processes count: %d\n", num_procs);
+    }
 	
-	MPI_Finalize();
-	return 0;
+    MPI_Finalize();
+    return 0;
 }
