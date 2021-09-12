63466faa93091c37bcaa9e6ee820a48146b11d1c
Преподаватель КАиТП 07 <pdinstructor07@calc.cod.phystech.edu>
2021-09-12 15:28:41 +0300

    run on cluster

--- sem01/5_time/run_cycle_sbatch.sh
+++ sem01/5_time/run_cycle_sbatch.sh
@@ -3,5 +3,5 @@ module add mpi/openmpi4-x86_64
 for p in {1..5}
     do
         echo "Processes count: $p"
-        sbatch -n $p --comment="Run cycle time task" ./a.out
+        sbatch -n $p --comment="Run cycle time task" run.sh
     done
